9640fd07080fb85fe9c3535eba8f973fe9e24d05
jnfrati <nicofrati@gmail.com>
2019-05-21 10:32:42 -0300

    Portfolio V1

--- src/App.vue
+++ src/App.vue
@@ -1,161 +1,86 @@
 <template>
 <div id="app">
-    <div class="container">
-
-    <header id="profilePhoto"><!--Profile photo-->
-      <img src="https://www.gravatar.com/avatar/3ba4f174206afc508dbe40945928431f?s=1024">
-      <h3>Nicolas Frati</h3>
-      <span><i>Developer</i></span>
+    <img src="@/assets/favicon-32x32.png"/>
+    <div id="floatingList" >
+      <!-- <a onClick="expandBrandList()"><span class="fas fa-link"></span></a> -->
+      <div id="listItems">
+        <a @click="changeLanguage"><span class="fas fa-language"></span></a> 
+        <a href="http://github.com/jnfrati" target="_blank"><span class="fab fa-github"></span></a>
+        <a href="https://www.instagram.com/nicofrati/" target="_blank" ><span class="fab fa-instagram"></span></a>
+        <a href="https://twitter.com/NFrati" target="_blank" ><span class="fab fa-twitter"></span></a>
+        <a href="https://www.linkedin.com/in/jnfrati" target="_blank"><span class="fab fa-linkedin"></span></a>
+        <a href="mailto: nicofrati@gmail.com" target="_blank"><span class="fas fa-envelope"></span></a>
+      </div>
+    </div>
+    <header><!--Profile photo-->
+      <div id="profile">
+        <img id="profilePhoto" src="https://www.gravatar.com/avatar/3ba4f174206afc508dbe40945928431f?s=1024">
+        <div id="profileDescription">
+          <h3>Nicolas Frati</h3>
+          <span><i>{{info.profileSpan}}</i></span>
+          <a href="https://drive.google.com/open?id=1OZ0OaNDnPskxcTQqQF1jqPKABjvWrxjJ" target="_blank">{{info.resume}}<i class="far fa-file-pdf"/></a>
+        </div>
+      </div>
     </header>
-      <div id="whoAmIBox" class="box"><!--Who am I-->
+  <div class="divisor"><span>{{info.aboutMeHeader}}</span><i class="fas fa-angle-down"></i></div>
+    <div class="container">
+    <div id="whoAmIBox" class="box"><!--Who I am-->
         <div class="box-title">
-          <p><b>Who am I?</b></p>
+          <p><b>{{info.whoAmITitle}}</b></p>
         </div>
         <div class="box-content">
-        <p>
-          This is a brief description of who am I. Nice to meet you. My a name is Nicolas Frati, I'm a full-stack junior developer. I'm a student in the 4th year of the bachelor sistem career. 
-          </p>
+          <p>{{info.whoAmI}}</p>
         </div>
       </div>  
     <div id="commonLinksBox" class="box"><!--Common Links-->
       <div class="box-title">
-        <p><b>Contact Me!</b></p>
+        <p><b>{{info.whatIDoTitle}}</b></p>
       </div>
       <div class="box-content">
-      <div class="list">
-        <a href="#" class="button"><span class="fab fa-github fa-2x"></span>Github</a>
-        <a href="https://www.instagram.com/nicofrati/" class="button"><span class="fab fa-instagram fa-2x"></span>Instagram</a>
-        <a href="https://twitter.com/NFrati" class="button"><span class="fab fa-twitter fa-2x"></span>Twitter</a>
-        <a href="https://twitter.com/NFrati" class="button"><span class="fab fa-linkedin fa-2x"></span>Linkedin</a>
-      </div>
+        <p>{{info.whatIDo}}</p>
       </div>
     </div>
   </div>
+  <div class="divisor"><span>{{info.projectsHeader}}</span><i class="fas fa-angle-down"></i></div>
+  <div class="work-cards">
+    <div class="project-box" v-for="project in info.projects" v-bind:key=project.name><!--My Projects-->
+        <div class="box-title">
+          <p><b>{{project.name}}</b></p>
+        </div>
+      <div class="box-buttons"><a :href="project.url" target="_blank"><span class="fas fa-external-link-alt"/></a></div> 
+    </div>
+  </div>
+  <footer>
+    <p>Frati Nicolas Portfolio. 2019</p>
+  </footer>
   </div>
 </template>
-
-<style>
-html, body, #app{
-  margin: 0px;
-  padding: 0px;
-  overflow: auto;
-}
-#app {
-  background: no-repeat linear-gradient(to bottom right,#0c0032, #190061);
-  background-attachment: fixed;
-  font-family: 'Roboto Slab', serif;
-  font-size: 10px;
-}
-.container{
-  margin: 20px auto;
-  padding: 0px;
-  width: 85%;
-  display: grid;
-  align-items: center;
-  grid-template-columns: 1fr;
-  /* grid-template-rows: 1fr 1fr 1fr; */
-  grid-auto-rows: auto;
-  grid-template-areas:  
-                        "profile"
-                        "whoami"
-                        "commonlink";
-    row-gap: 30px;
-}
-
-#profilePhoto {
-  grid-area: profile;
-  width: 100%;
-  text-align: center;
-  font-size: 1.6rem;
-  color: #f2f2f2;
-}
-
-#profilePhoto img {
-  display: block;
-  width: 100%;
-  max-width: 300px;
-  border-radius: 50%;
-  margin: 0 auto;
-}
-
-.box {
-  display:block;
-  margin: 0px auto;
-  width: 100%;
-  max-width: 400px;
-  background: #3523ff;
-  border-radius: 10px;
-}
-.box .box-title{
-  border-bottom: 1px #282828 solid;
-  text-align: center;
-  height: 10%;
-  padding: 1px;
-  font-size: 1.2rem;
-  background: #f64c72;
-  color: white;
-}
-.box .box-content{
-  font-size: 1rem;
-  margin: 10px;
-  padding-bottom: 3px;
-  color:black;
-}
-
-.list{
-  text-align:center;
-}
-.button{
-  text-decoration: none;
-  color: white;
-  display: inline-block;
-  margin: 5px auto;
-  padding: 2px;
-  width: 200px;
-  text-align: center;
-  background-color: #f64c72;
-  border: 1px #f64c72 solid;
-}
-.button span{
-  display: inline-block;
-  vertical-align: middle;
-  line-height:normal;
-  margin-right: 10px;
-}
-
-.button:hover{
-  border: 1px #f64c72 solid;
-  background: none;
-}
-
-#whoAmIBox{
-   grid-area: whoami;
-   display: block;
-   float:left;
-}
-#whoAmIBox .box-title{
-  border-top-right-radius: 10px;
-  border-bottom-left-radius: 10px;
-}
-#commonLinksBox{
-  grid-area: commonlink;
-  display: block;
-  float:right;
-}
-#commonLinksBox .box-title{
-  border-bottom-right-radius: 10px;
-  border-top-left-radius: 10px;
-}
-@media (min-width:1024px){
-  .container{
-    grid-template-columns: 1fr 1fr;
-    grid-template-areas:  
-                        "profile profile"
-                        "whoami . "
-                        ". commonlink";
-    column-gap: 0;
-    row-gap: 30px;
+<script>
+import myself from './myself.js'
+import './App.css';
+export default {
+  data(){
+    return({
+      info: myself.es
+    })
+  },
+  methods:{
+    changeLanguage: function(){
+      if(this.info.language == "ES"){
+        this.info = myself.en
+      }else{
+        this.info = myself.es
+      }
+    },
+    expandBrandList: ()=>{
+      var x = document.getElementById('listItems')
+      if(x.style.display == 'none' || x.style.display==''){
+        x.style.display = 'inherit';
+      }else{
+        x.style.display = "none";
+      }
+    }
   }
 }
+</script>
 
-</style>
---
+++ src/myself.js
@@ -0,0 +1,64 @@
+export default  {
+    es:{
+        language: "ES",
+        profileSpan: "Desarrollador Full-Stack",
+        resume:"Curriculum",
+        aboutMeHeader: "Sobre Mi",
+        whoAmITitle: "Quien soy",
+        whoAmI: "Hola! Soy Nicolas Frati, y tengo 21 años. Soy estudiante de la Universidad Nacional de Chilecito en Licenciatura en Sistemas. Estos últimos años mi pasión se enfocó en el desarrollo web, aunque la mayor parte de mis habilidades las desarrolle el último año. Me considero una persona de mente abierta, no tengo inconvenientes al escuchar las ideas de otras personas o aprender cosas nuevas para poder alcanzar un objetivo",
+        whatIDoTitle: "Que hago",
+        whatIDo:"En resumen, soy un programador, por ende, escribo codigo. Me considero un Full Stack junior luego de haber participado en el desarrollo de varios proyectos dentro del contexto academico. A la hora de trabajar en el Front-end prefiero trabajar con VueJS aunque tengo conocimientos en Angular y ReactJS. En el back end prefiero trabajar con NodeJS (Express o Graphql) pero he trabajado en PHP con Laravel y tuve un par de experiencias con Wordpress hace algunos años.",
+        projectsHeader: "Mis Proyectos",
+        projects:[
+            {
+                name:"Esta Pagina!",
+                url:"https://github.com/jnfrati/jnfrati.github.io"
+            },
+            {
+                name:"CODE Front-end",
+                url:"https://github.com/GrupoETICUndec/code-frontend"
+            },
+            {
+                name:"CODE Back-end",
+                url:"https://github.com/GrupoETICUndec/code"
+            },
+            {
+                name:"KIUS Front-end",
+                url:"https://github.com/kius-system/web-frontend"
+            },
+        ]
+    },
+    en:{
+        language: "EN",
+        profileSpan: "Full-Stack Developer",
+        resume:"Resume",
+        aboutMeHeader: "About Me",
+        whoAmITitle: "Who Am I",
+        whoAmI: "Hello! My name is Jose Nicolas Frati and I'm 21 yo. I'm a student at the University of Chilecito on Software Engineering. My passion over the last few years has been Web Development although I've developed most of my skill set the past year. I consider myself open minded, so I really don't have any problems when it comes to listening to other people's ideas or learning new things to achieve the target.",
+        whatIDoTitle: "What I Do",
+        whatIDo:"In short, I'm a programmer so I do code. I consider myself a Jr Full Stack developer after having participated in the  development of several projects at University. I work on the fron-end with VueJS, but I also have a basic knowledge of Angular and ReactJS. On the back end I prefer to work with NodeJS(Express on REST or with Graphql-yoga) but I've also worked with PHP on Laravel and had some experiences a couple of years ago on Wordpress",
+        projectsHeader: "My Projects",
+        projects:[
+            {
+                id: 1,
+                name:"This Web!",
+                url:"https://github.com/jnfrati/jnfrati.github.io"
+            },
+            {
+                id: 2,
+                name:"CODE Front-end",
+                url:"https://github.com/GrupoETICUndec/code-frontend"
+            },
+            {
+                id:3,
+                name:"CODE Back-end",
+                url:"https://github.com/GrupoETICUndec/code"
+            },
+            {
+                id: 4,
+                name:"KIUS Front-end",
+                url:"https://github.com/kius-system/web-frontend"
+            },
+        ]
+    }
+}
