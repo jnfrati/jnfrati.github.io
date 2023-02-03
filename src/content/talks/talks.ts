1d63c40a953dff9134c5c78542f9a65877bff4ad
jnfrati <nicofrati@gmail.com>
2023-02-02 21:01:14 -0300

    Implement talks section

---
+++ src/content/talks/talks.ts
@@ -0,0 +1,28 @@
+export const talks = [
+  {
+    name: "Decentralizing the storage with GunJS",
+    slug: "decentralizing-the-storage-with-gunjs",
+    url: "https://www.youtube.com/live/j4hJbsCePrQ?feature=share&t=793",
+    date: "2021-10-21",
+    org: "BeerJS",
+  },
+  {
+    name: "Vue 3 the revolution",
+    slug: "vue-3-the-revolution",
+    url: "https://www.youtube.com/live/EiVuLos1cyk?feature=share&t=649",
+    date: "2020-12-17",
+    org: "BeerJS",
+  },
+  {
+    name: "Introduction to Vue",
+    slug: "introduction-to-vue",
+    org: "Vue CBA",
+    date: "2020-04-30",
+  },
+  {
+    name: "Context API and how to use it",
+    slug: "context-api-and-how-to-use-it",
+    org: "React CBA",
+    date: "2020-01-30",
+  },
+];
